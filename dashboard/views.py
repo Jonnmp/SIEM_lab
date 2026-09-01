839598bab5ed0e348491fb1acd53c6ec1b1705d0
Jonathan Moo <250204001@escarcega.tecnm.mx>
2026-09-01 11:28:22 -0600

    Feat: Integra interfaz visual del monitor SOC (DASHBOARD)

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """
+    Renderiza el panel principal del SIEM.
+    Aquí enviaremos más adelante los datos de las alertas y sensores.
+    """
+    return render(request, 'dashboard/index.html')
